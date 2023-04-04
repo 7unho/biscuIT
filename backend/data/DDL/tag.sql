78fd522ddb479c59e22042b959c192df56074ea2
randallkk <randallkk.sw@gmail.com>
2023-04-05 04:40:12 +0900

    S08P22A706-416 fix: 파일명 바꾸기...

---
+++ backend/data/DDL/tag.sql
@@ -0,0 +1,14 @@
+insert into biscuit.tag (id, content_cnt, name, category_id)
+values  (8, 431, 'AI/ML', 1),
+        (9, 381, 'Frontend', 2),
+        (11, 420, 'Backend', 3),
+        (12, 364, 'Data', 4),
+        (13, 461, 'Infra', 5),
+        (14, 690, 'DevOps', 6),
+        (18, 85, 'Product', 7),
+        (19, 83, 'Conference', 8),
+        (25, 212, 'Mobile', 9),
+        (26, 170, 'Design', 10),
+        (28, 447, 'Culture', 11),
+        (45, 86, 'Security', null),
+        (207, 22, 'Embedded', null);
