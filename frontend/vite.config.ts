f5c1c97d492084ff1ed4421c0f4bf8e04b7ab24a
angielxx <angielxx94@gmail.com>
2023-04-05 14:56:14 +0900

    S08P22A706-424 fix: vite.config.ts 되돌리기

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
-import { ViteDevServer } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -24,38 +23,4 @@ export default defineConfig({
     viteTsconfigPaths(),
     svgrPlugin(),
   ],
-  server: {
-    proxy: {
-      '/kmong': {
-        target: 'https://blog.kmong.com',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/kmong/, ''),
-      },
-      '/sk': {
-        target: 'https://devocean.sk.com/blog/techBoardDetail.do',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/sk/, ''),
-      },
-      '/coupang': {
-        target: 'https://medium.com/coupang-engineering/',
-        changeOrigin: true,
-        secure: true,
-        rewrite: (path) => path.replace(/^\/coupang/, ''),
-      },
-      '/aws': {
-        target: 'https://aws.amazon.com/ko/blogs/',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/aws/, ''),
-      },
-      '/29cm': {
-        target: 'https://medium.com/29cm/',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/29cm/, ''),
-      },
-    },
-  },
 });
