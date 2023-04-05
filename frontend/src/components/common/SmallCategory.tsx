5d73e58b2cd914d881c22f2c3fb782a6f688db85
angielxx <angielxx94@gmail.com>
2023-04-05 15:43:51 +0900

    S08P22A706-422 fix: 타입스크립트 빌드 오류

--- frontend/src/components/common/SmallCategory.tsx
+++ frontend/src/components/common/SmallCategory.tsx
@@ -2,9 +2,9 @@
 import { useEffect, useState } from 'react';
 import tw, { styled } from 'twin.macro';
 
-const Category = ({ category }: { category: string }) => {
-  return <img src={`/src/assets/icons/category/${category}.svg`} />;
-};
+// const Category = ({ category }: { category: string }) => {
+//   return <img src={`/src/assets/icons/category/${category}.svg`} />;
+// };
 
 const CategoryBox = styled.li`
   ${tw`flex flex-col items-start order-5 w-full px-4 py-3 gap-[10px] text-white border-b border-solid border-dark-grey10 cursor-pointer`}
