5d11f8beece3fa609609457691bf0bcff213f1ea
angielxx <angielxx94@gmail.com>
2023-04-05 14:46:53 +0900

    S08P22A706-424 fix: 빌드 오류 수정 (프록시 패키지)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
-import { createProxyMiddleware, ViteDevServer } from 'vite';
+import { ViteDevServer } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
